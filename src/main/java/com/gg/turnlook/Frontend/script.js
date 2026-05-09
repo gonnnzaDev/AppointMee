2e850da582c3ae64c77709076cb8b92e07ccfc37
gonnnzadev <gonnnzadev@gmail.com>
2026-05-08 21:07:53 -0300

    arregle el navbar y lo mejore

--- src/main/java/com/gg/turnlook/Frontend/script.js
+++ src/main/java/com/gg/turnlook/Frontend/script.js
@@ -1,19 +1,17 @@
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regla q todos los email deben cumplir
 
-//Esto carga la parte principal O Login de una
-renderLogin();
-
-
-const renderNavbar = document.getElementById("navbar")
+const navbar = document.getElementById("navbar");
 //Esto es para modularizar el navbar
 
-if (renderNavbar) {
+if (navbar) {
 
     navbar.innerHTML = `
-<nav class="navbar border-bottom border-body" data-bs-theme="dark">
+
+<nav class="navbar navbar-expand-sm border-bottom border-body" data-bs-theme="dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
         <img src="https://cdn.discordapp.com/attachments/1492334072901533747/1502155047633424504/content.png?ex=69feae68&is=69fd5ce8&hm=373d791a61c3ae304c7d324ffe6da30d02ae77d062898b8284aacd23271ea4fb"></a>
+<h2>AppointMee</h2>
 
         <button class="navbar-toggler" type="button"
             data-bs-toggle="collapse"
@@ -22,6 +20,7 @@ if (renderNavbar) {
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-left" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M2 13.5a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 0-1H3.707L13.854 2.854a.5.5 0 0 0-.708-.708L3 12.293V7.5a.5.5 0 0 0-1 0z"/>
 </svg>
+
         </button>
 
         <div class="collapse navbar-collapse" id="navbarScroll">
@@ -30,6 +29,10 @@ if (renderNavbar) {
 
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Home</a>
+                </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">Profile</a>
                 </li>
 
                 <li class="nav-item">
@@ -52,6 +55,10 @@ if (renderNavbar) {
 `;
 }
 
+//Esto carga la parte principal O Login de una
+renderLogin();
+
+
 
 function renderLogin() {
 
@@ -64,7 +71,7 @@ function renderLogin() {
                     <img src="https://cdn.discordapp.com/attachments/1492334072901533747/1502155047633424504/content.png?ex=69feae68&is=69fd5ce8&hm=373d791a61c3ae304c7d324ffe6da30d02ae77d062898b8284aacd23271ea4fb"
                         alt="Login">
 
-                    <h1>AppointMe</h1>
+                    <h1>AppointMee</h1>
                     <div class="login-form">
                         <div class="input-group mb-1">
                             <input type="text" class="form-control" placeholder="Mail" aria-label="Username"
@@ -85,7 +92,6 @@ function renderLogin() {
                                 </svg>
                             </button>
                             <hr>
-                            </hr>
                             <button type="button" id="register-button">Register</button>
 
                         </div>
@@ -260,7 +266,7 @@ function renderRegister() {
 
 function postUser(name, surname, mail, pass) {
 
-    fetch("http://localhost:8080/", {
+    fetch("http://localhost:8080/usuarios/crear", {
 
         method: "POST",
 
@@ -269,8 +275,10 @@ function postUser(name, surname, mail, pass) {
         },
 
         body: JSON.stringify({
-            nombre: nombre,
-            apellido: apellido
+            nombre: name,
+            apellido: surname,
+            email: mail,
+            password: pass
         })
 
     })
@@ -288,6 +296,27 @@ function postUser(name, surname, mail, pass) {
 
 
 }
+
+
+//Perfil de usuario
+
+
+const infoAccountDiv = document.getElementById("info-account");
+
+infoAccountDiv.innerHTML = `    
+  <h1>My Account</h1>
+
+            <h3>Nombre: </h3>
+            <h3>Apellido: </h3>
+            <h3>Email: </h3>
+            <h3>Fecha De Creacion: </h3>
+
+            <img src="https://i.pinimg.com/236x/63/25/10/632510e53b3ae17f36993d7993c9fe8f.jpg" alt="">
+      `;
+
+
+
+
 
 
 
